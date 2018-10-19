6b85c7d50538f4d4c91620fc349ffa38afc00875
jancy-joseph <jancyworkmail@gmail.com>
2018-10-19 05:08:13 -0700

    Controller

---
+++ ProductCatalogAPI/Controllers/PicController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCatalogAPI.Controllers
+{
+    //  [Route("api/[controller]")]
+    //Added below two lines and commeneted the one above
+    [Produces("application/json")]
+    [Route("api/Pic")]
+    [ApiController]
+    public class PicController : ControllerBase
+    {
+        //local variable to capture hosting environment value from constructor
+        private readonly IHostingEnvironment _env;
+        public PicController(IHostingEnvironment env)//constructor
+        {
+            _env = env;
+        }
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webRoot = _env.WebRootPath;
+            var path = Path.Combine(webRoot + "/Pics/" + "shoes-" + id + ".png");
+            var buffer = System.IO.File.ReadAllBytes(path);
+            return File(buffer, "Image/png");
+
+        }
+    }
+}
--- ProductCatalogAPI/Program.cs
+++ ProductCatalogAPI/Program.cs
@@ -14,14 +14,19 @@ namespace ProductCatalogAPI
 {
     public class Program
     {
+        //Notes:IIS - Internet Information service - a piece of software  in windows that hosts web application.Similar to Tomcat in Linux
         public static void Main(string[] args)
         {
             var host = CreateWebHostBuilder(args).Build();
+            //Here we are creating a large object so we need to let garbage colelctor know we dont need this anymore.
+            //Here when we reach the end of using statement we dont want scope anymore as scope can be alarge object
             using (var scope = host.Services.CreateScope())//Wait until my Database is creted before seeding
             {
                 var services = scope.ServiceProvider;
                 var context = services.GetRequiredService<CatalogContext>();//tell me if DB's created by CatalogContext  is there
+                CatalogSeed.SeedAsync(context).Wait();
             }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
