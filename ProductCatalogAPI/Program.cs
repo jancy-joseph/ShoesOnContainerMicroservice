0ea23090abd5dbbb001af0d491659046ec1e9758
jancy-joseph <jancyworkmail@gmail.com>
2018-10-18 23:25:30 -0700

    Database creation and seeding

--- ProductCatalogAPI/Program.cs
+++ ProductCatalogAPI/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ProductCatalogAPI.Data;
 
 namespace ProductCatalogAPI
 {
@@ -14,7 +16,12 @@ namespace ProductCatalogAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())//Wait until my Database is creted before seeding
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<CatalogContext>();//tell me if DB's created by CatalogContext  is there
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
